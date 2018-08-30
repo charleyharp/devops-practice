270147d3643a7c52e3a1967c57e00269664f36f7
Charley Harp <charp@fenwaygroup.com>
2018-08-29 21:41:04 -0400

    finished tomcat wrapper

--- tomcat.sh
+++ tomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tomcat="../Documents/Tomcat/bin/"
-
+tomcatport="8090"
 if [ "$1" = "start" ]
 then
 	exec "${tomcat}startup.sh"
@@ -12,3 +12,14 @@ then
 	exec "${tomcat}shutdown.sh"
 fi
 
+if [ "$1" = "status" ]
+then
+	status=`nmap -p $tomcatport localhost`
+	echo $status
+	if [[ $status = *"closed"* ]];
+	then
+		echo "Not running on port $tomcatport"
+	else
+		echo "Running on port $tomcatport"
+	fi
+fi
